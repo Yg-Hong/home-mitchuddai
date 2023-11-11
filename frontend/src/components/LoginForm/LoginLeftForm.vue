4b8c8c5043aa85ec79f8212f9ab9cd04d97be381
lion0077v <lion0077v@gmail.com>
2023-11-11 16:45:36 +0900

    [edit] loginRightForm 완성

--- frontend/src/components/LoginForm/LoginLeftForm.vue
+++ frontend/src/components/LoginForm/LoginLeftForm.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <a-col :span="16"> Menu </a-col>
+  <a-col :span="auto"> Menu </a-col>
 </template>
 
 <style scoped></style>
--- frontend/src/components/LoginForm/LoginRightForm.vue
+++ frontend/src/components/LoginForm/LoginRightForm.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { reactive, computed } from "vue";
+import {
+  UserOutlined,
+  LockOutlined,
+  CaretRightOutlined,
+} from "@ant-design/icons-vue";
 
 const formState = reactive({
   userId: "",
@@ -23,7 +28,12 @@ const disabled = computed(() => {
 <template>
   <a-col :span="8">
     <div class="right">
-      <div class="fontA">로그인</div>
+      <div class="NexonFootballGothicBold FontL FontColorA">
+        Home Mitchuddai
+      </div>
+      <div class="NexonFootballGothicBold FontM FontColorB">
+        로그인 해주세요.
+      </div>
       <a-form
         :model="formState"
         name="normal_login"
@@ -32,51 +42,75 @@ const disabled = computed(() => {
         @finishFailed="onfinishFailed"
       >
         <a-form-item
-          label="userId"
           name="userId"
           :rules="[{ requried: true, messgae: 'Please input yout user ID!' }]"
         >
-          <a-input v-model:value="formState.userId">
+          <a-input
+            v-model:value="formState.userId"
+            placeholder="ID"
+            class="InputBox"
+          >
             <template #prefix>
-              <UserOutlined class="site-form0-item-icon" />
+              <UserOutlined class="site-form-item-icon" />
             </template>
           </a-input>
         </a-form-item>
 
         <a-form-item
-          label="Password"
           name="password"
           :rules="[{ required: true, message: 'Please input your password!' }]"
         >
-          <a-input-password v-model:value="formState.password">
+          <a-input-password
+            v-model:value="formState.password"
+            placeholder="Password"
+            class="InputBox"
+          >
             <template #prefix>
-              <LockOutlined class="site-fiorm-item-icon" />
+              <LockOutlined class="site-form-item-icon" />
             </template>
           </a-input-password>
         </a-form-item>
 
         <a-form-item>
-          <a-form-item name="remember" no-style>
-            <a-checkbox v-model:checked="formState.remember"
-              >Remeber me</a-checkbox
-            >
-          </a-form-item>
-          <a class="login-form-forgot" href="">Forgot Password</a>
+          <a-row justify="space-between">
+            <a-col :span="6">
+              <a-form-item name="remember" no-style>
+                <a-checkbox
+                  v-model:checked="formState.remember"
+                  class="NexonGothicBold FontColorA"
+                  >아이디 저장
+                </a-checkbox>
+              </a-form-item>
+            </a-col>
+            <a-col span="6">
+              <a-row justify="end">
+                <a class="NexonGothicBold FontColorC" href="">비밀번호 찾기</a>
+              </a-row>
+            </a-col>
+          </a-row>
         </a-form-item>
 
-        <a-form-item>
-          <a-button
-            :disabled="disabled"
-            type="primary"
-            html-type="submit"
-            class="login-form-button"
-          >
-            Log in
-          </a-button>
-          Or
-          <a href="">register now!</a>
-        </a-form-item>
+        <a-row justify="end">
+          <a-form-item>
+            <a-button
+              :disabled="disabled"
+              html-type="submit"
+              class="login-form-button"
+              id="login-form-button"
+            >
+              <span>Log in </span> <CaretRightOutlined />
+            </a-button>
+          </a-form-item>
+        </a-row>
       </a-form>
+
+      <a-flex :vertical="true" class="login-form-footer" justify="end">
+        <a-row justify="space-between">
+          <a-col :span="12"
+            ><a class="NexonGothicBold FontColorC" href="">회원가입 하기</a>
+          </a-col>
+        </a-row>
+      </a-flex>
     </div>
   </a-col>
 </template>
@@ -98,5 +132,27 @@ const disabled = computed(() => {
 }
 #components-form-demo-normal-login .login-form-button {
   width: 100px;
+}
+
+.InputBox {
+  height: 50px;
+}
+
+#login-form-button {
+  background-color: #002266;
+  color: #ffffff;
+
+  height: 40px;
+  width: 100px;
+}
+
+#login-form-button:hover {
+  background-color: #00387e;
+}
+
+.login-form-footer {
+  margin-top: 80px;
+
+  border-top: 1px solid;
 }
 </style>
