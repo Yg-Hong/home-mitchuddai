6e3968ac8790e31d90288739545b6cdd663e5a11
lion0077v <lion0077v@gmail.com>
2023-11-12 23:03:03 +0900

    [add] MainView 추가, routing 설정

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import WithoutLoginNavForm from "../components/nav/WithoutLoginNavForm.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <WithoutLoginNavForm />
+
+  <!-- 메인 폼 -->
+  <router-view />
+
+  <!-- 푸터 -->
+  <FotterForm />
+</template>
+
+<style scoped>
+.loginForm {
+  width: 1280px;
+}
+</style>
