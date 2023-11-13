9b54111ab64af541827e535b924c691fe8fb1e76
lion0077v <lion0077v@gmail.com>
2023-11-14 00:20:11 +0900

    [add] jpa entity mapping

--- backend/src/main/java/com/whereismyhome/house/house/dao/HouseDao.java
+++ backend/src/main/java/com/whereismyhome/house/house/dao/HouseDao.java
@@ -6,8 +6,9 @@ import java.util.Map;
 
 import com.whereismyhome.house.house.dto.DongCodeDto;
 import com.whereismyhome.house.house.dto.HouseDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HouseDao {
+public interface HouseDao extends JpaRepository {
 
 	List<DongCodeDto> dongCodeList(String dongName);
 
