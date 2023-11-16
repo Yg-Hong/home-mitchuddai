252a2d583157ebebf671296935fdee8ae1c8beb2
홍윤기 <lion0077v@gmail.com>
2023-11-16 15:23:20 +0900

    [add] house 컴포넌트 모두 완성

---
+++ frontend/src/components/HouseForm/HouseDetailCardForm.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  house: Object,
+});
+
+console.log(props.house);
+</script>
+
+<template>
+  <div class="houseComponent">
+    <a-row justify="start">
+      <a-col>{{ house.apartment }}</a-col>
+    </a-row>
+    <a-row justify="start">
+      <a-col>
+        {{ house.sidoName }} {{ house.gugunNAme }} {{ house.dongName }} {{ house.roadName }}
+      </a-col>
+    </a-row>
+    <a-row justify="start">
+      <a-col> 면적 : {{ house.area }} </a-col>
+    </a-row>
+    <a-row>
+      <a-col> 건축년도 : {{ house.buildYear }} </a-col>
+    </a-row>
+  </div>
+</template>
+
+<style scoped>
+.houseComponent {
+  width: 400px;
+
+  margin-bottom: 10px;
+
+  border: 1px solid black;
+  border-radius: 5px;
+}
+</style>
--- frontend/src/components/HouseForm/HouseForm.vue
+++ frontend/src/components/HouseForm/HouseForm.vue
@@ -1,18 +1,34 @@
 <script setup>
 import HouseListForm from "@/components/HouseForm/HouseListForm.vue";
 import KakaoMapForm from "./KakaoMapForm.vue";
+import { RouterView } from "vue-router";
+
+import { ref } from "vue";
+
+const latAndLngList = ref([]);
+
+const changeLatAndLngList = (newList) => {
+  console.log(newList);
+  latAndLngList.value = newList;
+};
 </script>
 
 <template>
   <a-row justify="center">
-    <a-col :span="8">
-      <HouseListForm />
+    <a-col :span="8" class="LeftForm">
+      <!-- <HouseListForm @changeLatAndLngList="changeLatAndLngList" /> -->
+      <RouterView />
     </a-col>
     <a-col :span="16">
-      <KakaoMapForm />
+      <KakaoMapForm :latAndLngList="latAndLngList" />
     </a-col>
   </a-row>
-  <div>ad</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.LeftForm {
+  padding-right: 30px;
+
+  border-right: 1px solid black;
+}
+</style>
