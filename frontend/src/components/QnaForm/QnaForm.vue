a5e489a8042f2a5ee8ebd099725ff28fd2dbe68b
홍윤기 <lion0077v@gmail.com>
2023-11-17 17:50:01 +0900

    [add] Qna Write Form 추가

--- frontend/src/components/QnaForm/QnaForm.vue
+++ frontend/src/components/QnaForm/QnaForm.vue
@@ -3,6 +3,10 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
+  <div class="QnaBoardTitle">
+    <a-row justify="center"> Qna게시판임요 </a-row>
+    <a-row justify="center"> 불만 있음 짱까시던가 </a-row>
+  </div>
   <RouterView />
 </template>
 
