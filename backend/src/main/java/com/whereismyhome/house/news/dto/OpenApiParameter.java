a4300232c9df7ea1d644ef42301f5d1de3a8ea97
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-11-20 12:57:00 +0900

    fix: 네이버 뉴스 api DTO 수정

--- backend/src/main/java/com/whereismyhome/house/news/dto/OpenApiParameter.java
+++ backend/src/main/java/com/whereismyhome/house/news/dto/OpenApiParameter.java
@@ -8,40 +8,6 @@ import lombok.*;
 public class OpenApiParameter {
 
     @Builder.Default
-    String realEstate = ""; //검색어: 부동산
-
-    @Builder.Default
-    String housing= ""; //검색어: 주택
-
-    @Builder.Default
-    String office= ""; //검색어: 사무실
-
-    @Builder.Default
-    String economicTrend= ""; //검색어: 경제동향
-
-    public String getRealEstate() {
-        return realEstate;
-    }
-    public void setRealEstate(String realEstate) {
-        this.realEstate = realEstate;
-    }
-    public String getHousing() {
-        return housing;
-    }
-    public void setHousing(String housing) {
-        this.housing = housing;
-    }
-    public String getOffice() {
-        return office;
-    }
-    public void setOffice(String office) {
-        this.office = office;
-    }
-    public String getEconomicTrend() {
-        return economicTrend;
-    }
-    public void setEconomicTrend(String economicTrend) {
-        this.economicTrend = economicTrend;
-    }
+    String keyword = ""; // 검색어
 
 }
