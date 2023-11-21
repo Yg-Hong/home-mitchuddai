8d16d28182e4cb4afa2804cf27ed82e5a27324c2
홍윤기 <lion0077v@gmail.com>
2023-11-21 16:20:06 +0900

    [add] 하단 컨텐츠 생성, new API 생성

--- backend/src/main/java/com/whereismyhome/house/news/controller/OpenApiController.java
+++ backend/src/main/java/com/whereismyhome/house/news/controller/OpenApiController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = { "*" }, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE} , maxAge = 6000)
+//@CrossOrigin(origins = { "*" }, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE} , maxAge = 6000)
 @RestController
 @RequestMapping("/open")
 @RequiredArgsConstructor
